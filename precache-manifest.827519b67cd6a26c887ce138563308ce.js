e3b5b07cdbf976b15859bd0b91f9c1059b1221eb
Yuta <mtsmtyuta@gmail.com>
2019-10-06 12:15:41 +0200

    Updates

---
+++ precache-manifest.827519b67cd6a26c887ce138563308ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b58faeface65bcc3f939ae5e5174f3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bdd4195e96a6897c4db3",
+    "url": "/monster-rolodex/static/css/main.54d52902.chunk.css"
+  },
+  {
+    "revision": "e57ff400467a2f25bbe9",
+    "url": "/monster-rolodex/static/js/2.1a8f040e.chunk.js"
+  },
+  {
+    "revision": "bdd4195e96a6897c4db3",
+    "url": "/monster-rolodex/static/js/main.ba158030.chunk.js"
+  },
+  {
+    "revision": "4ef5de2faf66d2334760",
+    "url": "/monster-rolodex/static/js/runtime-main.cc3a76ac.js"
+  }
+]);
